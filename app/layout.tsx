c45a307280f467d7d6038357f379410b7e8b7a25
SohailIIFL <Sohail.gidwani@iifl.com>
2025-05-18 01:35:54 +0530

    Add SpeedInsights package and integrate it into the layout for enhanced performance tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/app/globals.css'
 import { Inter } from 'next/font/google'
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>{children}</body>
       <Analytics />
+      <SpeedInsights />
     </html>
   )
 }
