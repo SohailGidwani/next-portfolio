9484093fa92b1a6ec40f9380fe1661c673bd4c54
SohailInsaito <sohail@insaito.com>
2025-07-05 22:02:29 +0530

    Refactor robots.txt configuration to use array format for rules and add host property

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,14 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        // disallow: ['/private/', '/admin/', '/api/'], // Uncomment if you have these in the future
+      },
+    ],
     sitemap: 'https://portfolio-sohail-gidwanis-projects.vercel.app/sitemap.xml',
+    host: 'https://portfolio-sohail-gidwanis-projects.vercel.app', 
   }
 } 
